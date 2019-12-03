23b13c03646f5af4ed048a10b336f8de99cf4a00
Tausif <engr.tausifkhan@gmail.com>
2019-12-03 23:16:45 +0000

    modified the conditions

--- timer1.js
+++ timer1.js
@@ -5,10 +5,13 @@ console.clear();
 let input1 = process.argv;
 const input = input1.slice(2);
 
-console.log(input)
+//console.log(input)
+let timeCounts = input.filter(function(ele) {
+  return !isNaN(Number(ele)) && ele >= 0;
+});
 
-for(let i = 0; i < input.length; i++){
-    setTimeout(()=>{
-        process.stdout.write('\x07');
-    }, Number(input[i]) * 1000);
+for (let i = 0; i < timeCounts.length; i++) {
+  setTimeout(() => {
+    process.stdout.write("\x07");
+  }, Number(timeCounts[i]) * 1000);
 }
